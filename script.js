9244d7aca26f228f46a6ed7a0feb23cbb635f647
nikhil-rawat02 <117645790+nikhil-rawat02@users.noreply.github.com>
2023-04-10 11:24:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,4 +2,12 @@
 
 let touristSpots = ['The Virupaksha Temple', 'Victoria Memorial', 'Tajmahal'];
 
-touristSpots.sort();
+let sortedBand = touristSpots.forEach(band => band.replace((a|an|the),'')).sort();
+
+let ui = document.getElementById("band");
+
+sortedBand.forEach(band => {
+	let li = document.createElement('li');
+	li.innerText = band;
+	ul.appendChild(li);
+});
