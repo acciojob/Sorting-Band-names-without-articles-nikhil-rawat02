8eeea5ea48c78e5a0398508c7f49f67beccf51d8
nikhil-rawat02 <117645790+nikhil-rawat02@users.noreply.github.com>
2023-04-10 17:54:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,11 +2,32 @@
 
 let touristSpots = ['The Virupaksha Temple', 'Victoria Memorial', 'Tajmahal'];
 
-let sortedBand = touristSpots.forEach(band => band.replace((a|an|the),'')).sort();
+// let sortedBand = touristSpots.forEach(band => band.replace(('a'),'')
+// 									 );
+function removeArticle(str){
+	let words = str.split(' ');
+
+	// words.map((word)=>{
+	// 	if()
+	// })
+	if(words.length > 1 && (words[0].toLowerCase()==='a' || words[0].toLowerCase()==='an' || words[0].toLowerCase()==='the')){
+		words.splice(0,1);
+	}
+	return words.join(" ");
+}
+
+touristSpots.sort((a,b)=>{
+	const bandA = removeArticle(a);
+	const bandB = removeArticle(b);
+
+	return bandA.localeCompare(bandB);
+})
+
+// console.log(touristSpots);
 
 let ui = document.getElementById("band");
 
-sortedBand.forEach(band => {
+touristSpots.forEach(band => {
 	let li = document.createElement('li');
 	li.innerText = band;
 	ul.appendChild(li);
